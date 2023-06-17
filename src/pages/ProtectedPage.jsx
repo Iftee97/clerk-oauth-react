9605ba176e6211f57756e4ff5dea2ea13cfb915b
Iftee97 <tawsifmasrur@gmail.com>
2023-06-17 06:31:55 +0600

    refactors

--- src/pages/ProtectedPage.jsx
+++ src/pages/ProtectedPage.jsx
@@ -11,13 +11,20 @@ export default function ProtectedPage() {
       <h1>
         Protected page
       </h1>
-      <div className='my-4'>
-        <p className='text-center text-lg mb-3'>
-          You are logged in as {' '}
-          <span className='text-blue-500'>
-            {user.firstName} {user.lastName}
-          </span>
-        </p>
+      <div className='my-6'>
+        <div className='flex items-center justify-center gap-3 mb-3'>
+          <p className='text-center text-lg'>
+            You are logged in as {' '}
+            <span className='text-blue-500'>
+              {user.firstName} {user.lastName}
+            </span>
+          </p>
+          <img
+            src={user?.imageUrl}
+            alt='user avatar'
+            className='w-10 h-10 rounded-full'
+          />
+        </div>
         <button
           className='text-lg text-red-500 hover:underline cursor-pointer'
           onClick={() => signOut()}
